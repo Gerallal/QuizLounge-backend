b3c70480cea6937e358aa67bacc50278b5aa98b2
Gerallal <dombrowe@th-brandenburg.de>
2025-11-04 10:16:16 +0100

    Initial commit

---
+++ src/main/java/gerallal/quizloungebackend/QuizLoungeBackendApplication.java
@@ -0,0 +1,13 @@
+package gerallal.quizloungebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizLoungeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizLoungeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gerallal/quizloungebackend/QuizLoungeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gerallal.quizloungebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizLoungeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
