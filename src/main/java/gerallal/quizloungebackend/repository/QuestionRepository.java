79c00d15d07013c48365d7f74169c719ef3847dc
hamms <hamms@th-brandenburg.de>
2025-12-28 15:38:24 +0100

    Results added when a quiz is completed and QuestionService expanded to include getNumberOfQuestions

--- src/main/java/gerallal/quizloungebackend/repository/QuestionRepository.java
+++ src/main/java/gerallal/quizloungebackend/repository/QuestionRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.repository.RepositoryDefinition;
 
 @RepositoryDefinition(domainClass = Quiz.class, idClass = Long.class)
 public interface QuestionRepository extends CrudRepository<Question, Long> {
+
+    int countByQuizId(Long quizId);
 }
